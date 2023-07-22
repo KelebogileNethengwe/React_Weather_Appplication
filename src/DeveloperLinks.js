591acdab6dba2526f876110e3987ef912dd54436
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-07-22 17:06:14 +0200

    Created Developer Links component
    Created Developer Links to code and LinkedIn profile

---
+++ src/DeveloperLinks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function DeveloperLinks() {
+    return (
+      <div className="DeveloperLinks">
+        Open{" "}
+        <a href="https://github.com/KelebogileNethengwe/React_Weather_App">
+          source code
+        </a>{" "}
+        by,
+        <a href="https://www.linkedin.com/in/kelebogile-nethengwe-5a8981212/">
+          Kelebogile Nethengwe
+        </a>
+      </div>
+    );
+}
