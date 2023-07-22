f2010d50bc3a1d40b954089544cb6cf8f623cd90
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-07-22 16:34:53 +0200

    Created Main Weather component

---
+++ src/MainWeather.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./MainWeather.css";
+
+export default function MainWeather() {
+  return (
+    <div className="MainWeatherInfo">
+      <h2 className="weatherCity">Johannesburg</h2>
+      <div id="datetime"></div>
+      <div>
+        <p id="description">Partly Cloudy</p>
+      </div>
+      <div className="weather_icon">
+        <img
+          src="https://openweathermap.org/img/wn/04d@2x.png"
+          alt="weather-icon"
+          id="icon"
+        />
+      </div>
+
+      <p>
+        <span id="weather-temperature">18</span>
+        <span class="unit">
+          <sup>
+            <a href="#" id="celsius">
+              ° C{" "}
+            </a>{" "}
+          </sup>
+        </span>
+      </p>
+    </div>
+  );
+}
