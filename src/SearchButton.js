fd2c1c97ed10bf068b33014b908eec9cf0742dbf
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-07-22 16:31:58 +0200

    Created SearchButton component

---
+++ src/SearchButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchButton.css";
+
+export default function SearchButton() {
+  return (
+    <button class="city-search-button">
+      <i className="fa-solid fa-magnifying-glass" value="submit"></i>
+    </button>
+  );
+}
