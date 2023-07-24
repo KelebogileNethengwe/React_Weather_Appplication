67c75f1e3f022d7aabcd2687aee86b3a68c4e7e2
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-07-24 15:04:30 +0200

    Deleted Components

--- src/CurrentLocationButton.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./CurrentLocationButton.css";
-
-export default function CurrentLocationButton() {
-  return <button className="current-location-button">current location</button>;
-}
--- src/WeeklyForecast.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./WeeklyForecast.css";
-
-export default function WeeklyForecast() {
-  return <div className="WeatherForecast row" id="forecast"></div>;
-}
