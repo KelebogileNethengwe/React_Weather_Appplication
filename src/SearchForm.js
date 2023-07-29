868c0e591a3910c2caa1ab1ccdd9a974531d5d86
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-07-29 11:49:56 +0200

    Updated Search Engine

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Circles } from "react-loader-spinner";
+import MainWeather from "./MainWeather";
 
 
 
 import "./App.css";
 
 export default function SearchForm(props) {
- 
+ const [city, setCity] = useState(props.defaultCity);
   const [weather, setWeather] = useState({loaded: false});
   function handleresponse(response) {
     console.log(response.data);
@@ -19,23 +20,48 @@ export default function SearchForm(props) {
       city: response.data.name,
       humidity: response.data.main.humidity,
       feelslike: response.data.main.feels_like,
+      date: new Date(response.data.dt *1000),
       description: response.data.weather[0].description,
       iconUrl: "https://openweathermap.org/img/wn/04d@2x.png",
     });
- 
+  
   }
+
+
+  function search() {
+ let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+ axios.get(apiUrl).then(handleresponse);
+  }
+
+
+
+function handleSubmit(event) {
+ 
+  event.preventDefault();
+  search()
+}
+
+
+
+function handleCityChange(event) {
+  setCity(event.target.value);
+}
+
 
   if (weather.loaded) {
     return (
       <div>
         <div className="SearchForm">
-          <form id="city-search-form">
+          <form id="city-search-form" onSubmit={handleSubmit}>
             <input
               className="CitySearch"
               type="text"
               id="input-search"
               placeholder="Search for a city"
               autocomplete="off"
+              onChange={handleCityChange}
             />
             <button class="city-search-button">
               <i className="fa-solid fa-magnifying-glass" value="submit"></i>
@@ -44,77 +70,14 @@ export default function SearchForm(props) {
               current location
             </button>
           </form>
+          <MainWeather data={weather} />
         </div>
-        <div className="MainWeatherInfo">
-          <h2 className="weatherCity">{weather.city}</h2>
-          <div id="datetime"></div>
-          <div>
-            <p id="description">Partly Cloudy</p>
-          </div>
-          <div className="weather_icon">
-            <img
-              src={weather.iconUrl}
-              alt={weather.description}
-              id="icon"
-            />
-          </div>
-
-          <p>
-            <span id="weather-temperature">
-              {Math.round(weather.temperature)}
-            </span>
-            <span class="unit">
-              <sup>
-                <a
-                  href="https://github.com/KelebogileNethengwe/React_Weather_App/tree/main"
-                  id="celsius"
-                >
-                  ° C{" "}
-                </a>{" "}
-              </sup>
-            </span>
-          </p>
-        </div>
-
-        <div className="weather_info row">
-          <div className="weather_card col">
-            <i className="fa-solid fa-droplet"></i>
-            <div>
-              <p>Humidity</p>
-              <p>
-                <span id="humidity">{weather.humidity}</span> %
-              </p>
-            </div>
-          </div>
-          <div className="weather_card col">
-            <i className="fa-solid fa-temperature-quarter"></i>
-            <div>
-              <p>Feels like</p>
-              <p>
-                <span id="feels_like">{weather.feelslike}</span> <sup>°</sup>
-              </p>
-            </div>
-          </div>
-          <div className="weather_card col">
-            <i className="fa-solid fa-wind"></i>
-            <div>
-              <p>Wind</p>
-              <p>
-                <span id="wind">{weather.wind}</span> km/h
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="WeatherForecast row" id="forecast"></div>
       </div>
     );
   } else {
+    search();
 
-   
-    let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleresponse);
+  
     return (
       <Circles
         height="80"
