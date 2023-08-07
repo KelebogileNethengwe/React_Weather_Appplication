1bbfe75687b79cd1c5740ccbc200418f9879521b
Kelebogile Nethengwe <105043013+KelebogileNethengwe@users.noreply.github.com>
2023-08-07 16:03:55 +0200

    Updated links

--- src/DeveloperLinks.js
+++ src/DeveloperLinks.js
@@ -5,11 +5,18 @@ export default function DeveloperLinks() {
     return (
       <div className="DeveloperLinks">
         Open{" "}
-        <a href="https://github.com/KelebogileNethengwe/React_Weather_App">
+        <a
+          href="https://github.com/KelebogileNethengwe/React_Weather_App"
+          rel="noopener noreffer"
+        >
           source code
-        </a>{" "}by{" "}
-        <a href="https://www.linkedin.com/in/kelebogile-nethengwe-5a8981212/">
-           Kelebogile Nethengwe
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/kelebogile-nethengwe-5a8981212/"
+          rel="noopener noreffer"
+        >
+          Kelebogile Nethengwe
         </a>
       </div>
     );
